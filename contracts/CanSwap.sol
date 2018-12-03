9cbad672fc36b13b9605981b6e6e10ecbb2745b8
alsco77 <alsco77@pm.me>
2018-12-03 14:47:10 +0800

    Implemented pool creation

--- contracts/CanSwap.sol
+++ contracts/CanSwap.sol
@@ -25,9 +25,14 @@ contract CanSwap is Ownable {
         bool blacklisted;
     }
 
-    struct PoolBalances {
+    struct PoolBalance {
         uint128 balTKN;
         uint128 balCAN;
+    }
+
+    struct PoolStake {
+        uint128 stakeTKN;
+        uint128 stakeCAN;
     }
 
     struct PoolFees {
@@ -37,13 +42,17 @@ contract CanSwap is Ownable {
 
     ERC20 public CAN;
 
-    uint8 public poolCount = 0;
+    uint16 public poolCount = 0;
+    mapping(uint16 => address) mapIndexToPool;
 
-    mapping(uint8 => address) mapIndexToPool;
     mapping(address => PoolDetails) mapPoolDetails;
     mapping(address => PoolStatus) mapPoolStatus;
-    mapping(address => PoolBalances) mapPoolBalances;
+    mapping(address => PoolBalance) mapPoolBalances;
     mapping(address => PoolFees) mapPoolFees; 
+
+    mapping(address => uint16) mapPoolStakerCount;
+    mapping(address => mapping(uint16 => address)) mapPoolStakerAddress;
+    mapping(address => mapping(address => PoolStake)) mapPoolStakes;
 
 
     constructor (address _canToken) public {
@@ -67,27 +76,48 @@ contract CanSwap is Ownable {
     }
 
     /**
-     * @dev Create a liquidity pool paired with CAN 
-     * It should:
+     * @dev Create a liquidity pool paired with CAN and perform initial stake
      */
     function createPoolForToken(address _token, string calldata _uri, string calldata _api, 
     uint128 _amountTkn, uint128 _amountCan) external payable {
         require(mapPoolStatus[_token].exists == false, "Pool must not exist");
-        require(_amountTkn > 0, "Pool must receive initial TKN stake");
-        require(_amountCan > 0, "Pool must receive initial CAN stake");
-        
-        // Handle ETH? ETHToken, second function, if block
-        ERC20 token = ERC20(_token);                                          
-        require(token.transferFrom(msg.sender, address(this), _amountTkn), "Must be able to transfer tokens from pool creator to pool");           
-        require(CAN.transferFrom(msg.sender, address(this), _amountCan), "Must be able to transfer CAN from pool creator to pool");             
         
         // Handle stakes, pool share, etc
-
         mapIndexToPool[poolCount] = _token;
         mapPoolDetails[_token] = PoolDetails(_uri, _api);
-        mapPoolBalances[_token] = PoolBalances(_amountTkn, _amountCan);
+        mapPoolStatus[_token] = PoolStatus(true, true, false);
         mapPoolFees[_token] = PoolFees(0, 0);
         poolCount += 1;
 
+        require(stakeInPool(_token, _amountTkn, _amountCan), "Stake must be successful");
+    }
+
+    /**
+     * @dev Perform stake in pool
+     */
+    function stakeInPool(address _token, uint128 _amountTkn, uint128 _amountCan) public 
+    payable
+    poolIsActive(_token) 
+    returns (bool success) {
+        require(_amountTkn > 0, "Pool must receive initial TKN stake");
+        require(_amountCan > 0, "Pool must receive initial CAN stake");
+        
+        if(_token == address(0)){
+            require(msg.value == _amountTkn, "Pool creator must send ETH stake");
+        } else {
+            ERC20 token = ERC20(_token);                                          
+            require(token.transferFrom(msg.sender, address(this), _amountTkn), "Must be able to transfer tokens from pool creator to pool");    
+        }
+        require(CAN.transferFrom(msg.sender, address(this), _amountCan), "Must be able to transfer CAN from pool creator to pool");             
+        
+        PoolBalance memory currentBalance = mapPoolBalances[_token];
+        mapPoolBalances[_token] = PoolBalance(currentBalance.balTKN += _amountTkn, currentBalance.balCAN += _amountCan);
+        
+        mapPoolStakerAddress[_token][mapPoolStakerCount[_token]] = msg.sender;
+        mapPoolStakerCount[_token] += 1;
+        mapPoolStakes[_token][msg.sender] = PoolStake(_amountTkn, _amountCan);
+        
+
+        return true;
     }
 }
