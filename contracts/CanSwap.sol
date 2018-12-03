12cea5c6a7aef9c0809657357212e00a9c454d52
alsco77 <alsco77@pm.me>
2018-12-03 16:32:07 +0800

    Implemented first iteration of swap

--- contracts/CanSwap.sol
+++ contracts/CanSwap.sol
@@ -40,6 +40,10 @@ contract CanSwap is Ownable {
         uint128 feeCAN;
     }
 
+    event eventCreatedPool(address indexed token, string uri, string api); 
+    event eventStake(address indexed token, uint128 amountTkn, uint128 amountCan);  
+    
+
     ERC20 public CAN;
 
     uint16 public poolCount = 0;
@@ -71,7 +75,7 @@ contract CanSwap is Ownable {
       * @dev Modifier - requires pool to be active
       */
     modifier poolIsActive(address _token) {
-        require(mapPoolStatus[_token].active, "Pool must be active");
+        require(mapPoolStatus[_token].active || _token == address(CAN), "Pool must be active");
         _;
     }
 
@@ -79,15 +83,16 @@ contract CanSwap is Ownable {
      * @dev Create a liquidity pool paired with CAN and perform initial stake
      */
     function createPoolForToken(address _token, string calldata _uri, string calldata _api, 
-    uint128 _amountTkn, uint128 _amountCan) external payable {
+    uint128 _amountTkn, uint128 _amountCan) 
+    external 
+    payable {
         require(mapPoolStatus[_token].exists == false, "Pool must not exist");
         
-        // Handle stakes, pool share, etc
         mapIndexToPool[poolCount] = _token;
         mapPoolDetails[_token] = PoolDetails(_uri, _api);
         mapPoolStatus[_token] = PoolStatus(true, true, false);
-        mapPoolFees[_token] = PoolFees(0, 0);
         poolCount += 1;
+        emit eventCreatedPool(_token, _uri, _api);
 
         require(stakeInPool(_token, _amountTkn, _amountCan), "Stake must be successful");
     }
@@ -95,7 +100,8 @@ contract CanSwap is Ownable {
     /**
      * @dev Perform stake in pool
      */
-    function stakeInPool(address _token, uint128 _amountTkn, uint128 _amountCan) public 
+    function stakeInPool(address _token, uint128 _amountTkn, uint128 _amountCan) 
+    public
     payable
     poolIsActive(_token) 
     returns (bool success) {
@@ -117,7 +123,73 @@ contract CanSwap is Ownable {
         mapPoolStakerCount[_token] += 1;
         mapPoolStakes[_token][msg.sender] = PoolStake(_amountTkn, _amountCan);
         
-
+        emit eventStake(_token, _amountTkn, _amountCan);
         return true;
     }
+
+    /**
+     * @dev Perform swap and return funds to sender
+     */
+    function swap(address _from, address _to, uint256 _value) 
+    public 
+    payable {
+        _swapAndSend(_from, _to, _value, msg.sender);
+    }
+        
+    /**
+     * @dev Perform swap and return funds to recipient
+     */
+    function swapAndSend(address _from, address _to, uint256 _value, address payable _recipient) 
+    public 
+    payable {
+        require(_recipient != address(0), "Recipient must be non empty address");
+        _swapAndSend(_from, _to, _value, _recipient);
+    }
+    
+    /**
+     * @dev Internal swap function
+     */
+    function _swapAndSend(address _from, address _to, uint256 _value, address payable _recipient) 
+    internal
+    poolIsActive(_from)
+    poolIsActive(_to) {        
+        require(_value > 0, "Must be attempting to swap a non zero amount of tokens");
+
+
+        if(_from == address(0)){
+            require(msg.value == _value);                    
+        } else {
+            ERC20 token = ERC20(_from);                                        
+            require(token.transferFrom(msg.sender, address(this), _value));
+        }
+
+        uint128 swapOutput;
+
+        if(_from == address(CAN) || _to == address(CAN)){
+            swapOutput = _executeSwap(_from, _to, _value);
+        } else {
+            initialSwapOutput = _executeSwap(_from, address(CAN), _value);
+            swapOutput = _executeSwap(address(CAN), _to, initalSwapOutput);
+        }
+
+        require(swapOutput > 0, "Must be some swap output");
+
+        if(_to == address(0)) {
+            _recipient.transfer(swapOutput);
+        }else {
+            ERC20 outputToken = ERC20(_to);
+            outputToken.transfer(_recipient, swapOutput);
+        }
+    }
+
+    function _executeSwap(address _from, address _to, )
+    internal
+    returns (uint128 swapOutput)
+    {
+        bool fromCan = _from == address(CAN);
+        // TODO - Ensure pool has adequate liquidity
+        // handle ETH
+    }
+    
+ 
 }
